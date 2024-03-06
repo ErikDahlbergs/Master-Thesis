a2b730936c1793246af16e84a04f1f8d61df1b31
ErikDahlbergs <er8251da-s@student.lu.se>
2024-03-06 16:40:10 +0100

    Relocation of images

--- Models/dcgan.py
+++ Models/dcgan.py
@@ -15,7 +15,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-os.makedirs("images", exist_ok=True)
+os.makedirs("images/DCGANs", exist_ok=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
@@ -203,6 +203,6 @@ for epoch in range(opt.n_epochs):
 
         batches_done = epoch * len(dataloader) + i
         if batches_done % opt.sample_interval == 0:
-            save_image(gen_imgs.data[:25], "images/%d.png" % batches_done, nrow=5, normalize=True)
+            save_image(gen_imgs.data[:25], "images/DCGANs/%d.png" % batches_done, nrow=5, normalize=True)
 
 torch.save(generator.state_dict(),"Saves/DCGAN.pt")
